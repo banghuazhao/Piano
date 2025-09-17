ef6b3b1e021b3b2bd23bc8fa25c31bf30c0f5076
Banghua Zhao <banghua.zhao@gmail.com>
2025-09-17 22:45:10 +1200

    Add ads

--- Piano/App/PianoApp.swift
+++ Piano/App/PianoApp.swift
@@ -2,15 +2,34 @@
 // Created by Banghua Zhao on 04/09/2025
 // Copyright Apps Bay Limited. All rights reserved.
 //
-  
 
+import GoogleMobileAds
 import SwiftUI
 
 @main
 struct PianoApp: App {
+    @StateObject private var openAd = OpenAd()
+    @Environment(\.scenePhase) private var scenePhase
+
+    init() {
+        MobileAds.shared.start(completionHandler: nil)
+    }
+
     var body: some Scene {
         WindowGroup {
             PianoView()
+                .onChange(of: scenePhase) { _, newPhase in
+                    print("scenePhase: \(newPhase)")
+                    if newPhase == .active {
+                        openAd.tryToPresentAd()
+                        openAd.appHasEnterBackgroundBefore = false
+                    } else if newPhase == .background {
+                        openAd.appHasEnterBackgroundBefore = true
+                    }
+                }
+                .onAppear {
+                    AdManager.requestATTPermission(with: 3)
+                }
         }
     }
 }
